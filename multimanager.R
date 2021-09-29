743d89448a642cb654acc8e92a8f1cb9263480fe
Alex Barth <abarth1014@gmail.com>
2021-09-29 17:48:30 -0400

    Pushed multimanager to github

---
+++ multimanager.R
@@ -0,0 +1,18 @@
+#' MultipleTool Manager
+#' 
+#' This function is a tool which facilitates quick renaming of vignettes with multiple organisms
+#' It will select all multiple-object vignettes then request user input for the names of the new file
+#' For each ID entered, it will create a new line
+#' 
+#' @param path location of file to choose, if not entered, a box will open to select file
+#' @param morphoInclude if true, will create a column of boolean values. True indicates that row will be included in morpho-measurements. New rows from multimanager default to F.
+#' @param openImage if true, will find and open images. Requires file connection
+#' @param imagePath if open image is true, a path to the image folder must be provided
+
+multiManager = function(path, morphoInclude = T, openImage = T, imagePath = NULL){
+  if(length(imagePath) == 0 & openImage == T){
+    stop("imagePath not provided")
+  }
+  ogDf = ecoImport(path,trim = F) #open originalDf
+
+}
