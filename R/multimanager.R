9efb82aaa8117c89bda31a09aa594e15302d2599
Alex Barth <abarth1014@gmail.com>
2021-10-02 00:12:13 -0400

    Making progress finished initial build of tsvtrim made progress on multimanager delected man folder for ecoImport

--- R/multimanager.R
+++ R/multimanager.R
@@ -1,3 +1,44 @@
+#' findMultiple - locate all multiple cateogories
+#' 
+#' Helper function to identify all types of multiple classes in 
+#' object_annotation_category
+#' 
+#' @param df a data frame with object_annotation_category
+#' 
+findMultiple = function(df){
+  taxaNames = unique(df$object_annotation_category)
+  if(length(taxaNames) == 0){
+    stop("No taxa names provided")
+  } else {
+    mults = grep("^multiple<",taxaNames)
+  }
+  if(length(mults) == 0){
+    stop("No multiples found")
+  } else{
+    return(taxaNames[mults])
+  }
+}
+
+#' getMultiIndex - helper function for user input
+#' 
+#' This calls for user input then asks which multiples to include
+#' then it provides the index for all multiples of interest
+#' 
+getSelection = function(df){
+  idMult = findMultiple(df)
+  userChoice = menu(c(idMult,"All"), title = cat("Found these multipe labels.","Select one?",
+                             sep = "\n"))
+  if(userChoice > length(idMult)){
+    idMult = idMult
+    
+  } else {
+    idMult = idMult[userChoice]
+    
+  }
+  index = which(df$object_annotation_category %in% idMult)
+  return(index)
+}
+
 #' multiManager - a tool to re-label vignettes with multiple individuals
 #' 
 #' This function is a tool which facilitates quick renaming of vignettes 
@@ -7,18 +48,30 @@
 #' For each ID entered, it will create a new line with that estimate
 #' Note that there will 
 #' 
+#' @importFrom ecotaxar -read_ecotaxa_tsv
+#' 
 #' @param path location of file to choose, if not entered, a box will open to select file
 #' @param morphoInclude if true, will create a column of boolean values. True indicates that row will be included in morpho-measurements. New rows from multimanager default to F.
 #' @param openImage if true, will find and open images. Requires file connection
 #' @param imagePath if open image is true, a path to the image folder must be provided
+#' @param removeMultiple if true, this will delete the multiple< lines
+#' 
 
-multiManager = function(path, morphoInclude = T, openImage = T, 
+multiManager = function(path, morphoInclude = T, openImage = T,
                         imagePath = NULL){
+  #possible errors
   if(length(imagePath) == 0 & openImage == T){
     stop("imagePath not provided")
   }
-  ogDf = ecoImport(path,trim = F) #open originalDf
+  ogDf = read_ecotaxa_tsv(path,trim = F) #open originalDf
+  outDf = ogDf # replicate output df
   
-  multiDf
+  if(morphoInclude == T){
+    outDf$morphoInclude = rep(T, nrow(outDf))
+  }
+  
+  
 
 }
+
+
--- R/tsvTrim.R
+++ R/tsvTrim.R
@@ -13,16 +13,28 @@ addCustomTrim = function(){
 vectorSelect = function(trimMode){
   
   necessary = c("object_id","object_lat","object_lon",
-                "object_date","object_time","object_depth_max",
-                "object_annotation_category")
+                "object_date","object_time","object_depth_min",
+                "object_depth_max","object_annotation_category")
   
   if(trimMode == "Minimal"){
     output = necessary
   } else if (trimMode == "Morpho"){
-    output = c(necessary,"object_major","object_minor",
-               "object_area", "object_feret","object_perim.","object_circ.",
-               "object_elongation","object_thickr", 'object_ver.sym', "object_major",
-               'object_minor', "object_feret", "object_esd")
+    output = c(necessary,"object_area","object_mean","object_stddev",
+               "object_mode","object_min","object_max","object_perim.",
+               "object_major","object_minor","object_circ.","object_feret",
+               "object_intden","object_median","object_skew","objet_kurt",
+               "object_%area","object_area_exc","object_fractal",
+               "object_skelarea","object_slope","object_histcum1",
+               "object_histcum2","object_histcum3","object_symetrieh",
+               "object_symetriev","object_symetriehc","objet_symetrievc",
+               "object_convperim","object_convarea","object_fcons",
+               "object_thickr","object_esd","object_elongation","object_range",
+               "object_cv","object_sr", "object_perimferet","object_perimmajor",
+               "process_pixel")
+  } else if(trimMode == "SampleInfo"){
+    output = c(necessary, "sample_zmax","sample_zmin","sample_tot_vol")
+  } else {
+    stop("No valid trimMode provided, Choose Minimal,Morpho,SampleInfo")
   }
   return(output)
 }
@@ -39,6 +51,16 @@ vectorSelect = function(trimMode){
 #' @param customVector if "Custom" trimMode provide a vector of column names
 #'
 tsvTrim = function(df, trimMode = "Minimal", customVector = NULL){
-  trimVect = vectorSelect(trimMode)
+  if(trimMode == "Custom" & length(customVector) == 0){
+    stop("No custom vector provided")
+  } else if(trimMode == "Custom" & length(customVector) != 0){
+    trimVect = customVector
+  } else{
+    trimVect = vectorSelect(trimMode)
+  } 
+  
+  df$process_pixel = df[,which(names(df)%in%c("process_particle_pixel_size_mm",
+                                            "process_pixel"))][1]
   tdf = df[,which(colnames(df) %in% trimVect)]
+  return(tdf)
 }
