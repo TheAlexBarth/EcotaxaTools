bc63dc6f12cce464521cc908828e1127d2eefac0
Alex Barth <AB93@email.sc.edu>
2022-01-14 11:13:21 -0500

    added depth col to both cases

--- R/binByTools.R
+++ R/binByTools.R
@@ -20,6 +20,7 @@ bin_by <- function(df, method, custom_range = NULL, equal_step = 10,
                          "Equal Number" = equal_number)
     return(cut(df[,depthcol], breaks = break_vect)) #cut into character vector
   } else if (method == "Equal Step"){
+    depthcol <- get_col_name(df,"depth_offset") #get the column for UVP
     bins <- seq(0,max(df[,depthcol])+equal_step,equal_step)#set up bin sequence
     set_bins <- unlist(lapply(nearest(df[,depthcol],bins))) #choose nearest bin
   }
