1cb28782d63aa3a0420a559abaf34ebfffd30e5d
Alex Barth <AB93@email.sc.edu>
2022-02-08 11:22:38 -0500

    added error message

--- R/binByTools.R
+++ R/binByTools.R
@@ -24,6 +24,9 @@ bin_by <- function(df, method, custom_range = NULL, equal_step = 10,
     bins <- seq(0,max(df[,depthcol])+equal_step,equal_step)#set up bin sequence
     set_bins <- unlist(lapply(df[,depthcol],nearest,bins)) #choose nearest bin
     return(set_bins)
+  } else {
+    stop("Error: Invalid method arguement. Choose from 'Zooscan','Equal Number',
+         'Custom', or 'Equal Step'.")
   }
 }
 
