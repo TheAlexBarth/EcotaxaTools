8ed08efe9dc7227e089857bab61fb01dca868516
Alex Barth <abarth1014@gmail.com>
2022-08-23 18:54:28 -0400

    reformat

--- R/ecopart_par_conc.R
+++ R/ecopart_par_conc.R
@@ -1,3 +1,96 @@
+
+
+#' Calculate particle concentration
+#' 
+#' caculate the particle concentration for a given size class
+#' The bulk of this code lives in \code{calc_par_conc} and this is a wrapper
+#' 
+#' @inheritParams calc_par_conc
+#' @param bin_limits optional for returning size-binned particle concentration
+#' @export
+uvp_par_conc <- function(par, 
+                         min_esd = 0, 
+                         max_esd = 100, 
+                         pixel_mm = NULL, 
+                         img_vol = NULL,
+                         bin_limits = NULL) {
+  if(is.etx_class(par,'ecopart_obj')) {
+    
+    if(length(unique(par$meta$default_instrumsn)) > 1){
+      warning('There are multiple sn\'s. Pixel size and img_vol will be wrong.
+              Consider split_ecopart_obj by instrument serial number.')
+    }
+    
+    pixel_mm <- unique(par$meta$acq_pixel)[1]
+    img_vol <- unique(par$meta$acq_volimage)[1]
+    
+    #if no bin_limits are provided, use total method
+    if(is.null(bin_limits)) {
+      par_sum_list <- lapply(par$par_files,
+                             calc_par_conc,
+                             min_esd = min_esd,
+                             max_esd = max_esd,
+                             pixel_mm = pixel_mm,
+                             img_vol = img_vol)
+    } else {
+      par_sum_list <- lapply(par$par_files,
+                             calc_par_conc_bin,
+                             bin_limits = bin_limits,
+                             pixel_mm = pixel_mm,
+                             img_vol = img_vol)
+    }
+    names(par_sum_list) <- names(par$par_files)
+    class(par_sum_list) <- c('list', 'par_conc_list')
+    return(par_sum_list)
+    
+  } else if (is.etx_class(par, 'par_list')) {
+    
+    if(is.null(pixel_mm) | is.null(img_vol)){
+      stop('Must provide either ecopart_obj or 
+           manually select pixel size and img vol')
+    }
+    
+    #if no bin_limits provided, use total method
+    if(is.null(bin_limits)) {
+      par_sum_list <- lapply(par,
+                             calc_par_conc,
+                             min_esd = min_esd,
+                             max_esd = max_esd,
+                             pixel_mm = pixel_mm,
+                             img_vol = img_vol)
+    } else {
+      par_sum_list <- lapply(par,
+                             calc_par_conc_bin,
+                             bin_limits = bin_limits,
+                             pixel_mm = pixel_mm,
+                             img_vol = img_vol)
+    }    
+    names(par_sum_list) <- names(par)
+    class(par_sum_list) <- c('list', 'par_conc_list')
+    return(par_sum_list)
+    
+  } else if(is.etx_class(par, 'par_df')) {
+    
+    if(is.null(pixel_mm) | is.null(img_vol)){
+      stop('Must provide either ecopart_obj or 
+           manually select pixel size and img vol')
+    }
+    #if no bin_limits provided use total method
+    if(is.null(bin_limits)) {
+      par_sum_df <- par |> calc_par_conc(min_esd = min_esd,
+                                         max_esd = max_esd,
+                                         pixel_mm = pixel_mm,
+                                         img_vol = img_vol)
+    } else {
+      par_sum_df <- par |> calc_par_conc_bin(bin_limits = bin_limits,
+                                             pixel_mm = pixel_mm,
+                                             img_vol = img_vol)
+    }
+    return(par_sum_df)
+  }
+}
+
+
 #' Add particle zeros
 #' 
 #' Just an inside function I wrote at 2am in the middle of the pacific
@@ -123,95 +216,4 @@ calc_par_conc_bin <- function(par,
                        FUN = sum) |> force_par_zeros()
   class(par_sum) <- c(class(par_sum), 'par_conc_df')
   return(par_sum)
-}
-
-
-#' Calculate particle concentration
-#' 
-#' caculate the particle concentration for a given size class
-#' The bulk of this code lives in \code{calc_par_conc} and this is a wrapper
-#' 
-#' @inheritParams calc_par_conc
-#' @param bin_limits optional for returning size-binned particle concentration
-#' @export
-uvp_par_conc <- function(par, 
-                         min_esd = 0, 
-                         max_esd = 100, 
-                         pixel_mm = NULL, 
-                         img_vol = NULL,
-                         bin_limits = NULL) {
-  if(is.etx_class(par,'ecopart_obj')) {
-    
-    if(length(unique(par$meta$default_instrumsn)) > 1){
-      warning('There are multiple sn\'s. Pixel size and img_vol will be wrong.
-              Consider split_ecopart_obj by instrument serial number.')
-    }
-    
-    pixel_mm <- unique(par$meta$acq_pixel)[1]
-    img_vol <- unique(par$meta$acq_volimage)[1]
-    
-    #if no bin_limits are provided, use total method
-    if(is.null(bin_limits)) {
-      par_sum_list <- lapply(par$par_files,
-                             calc_par_conc,
-                             min_esd = min_esd,
-                             max_esd = max_esd,
-                             pixel_mm = pixel_mm,
-                             img_vol = img_vol)
-    } else {
-      par_sum_list <- lapply(par$par_files,
-                             calc_par_conc_bin,
-                             bin_limits = bin_limits,
-                             pixel_mm = pixel_mm,
-                             img_vol = img_vol)
-    }
-    names(par_sum_list) <- names(par$par_files)
-    class(par_sum_list) <- c('list', 'par_conc_list')
-    return(par_sum_list)
-    
-  } else if (is.etx_class(par, 'par_list')) {
-    
-    if(is.null(pixel_mm) | is.null(img_vol)){
-      stop('Must provide either ecopart_obj or 
-           manually select pixel size and img vol')
-    }
-    
-    #if no bin_limits provided, use total method
-    if(is.null(bin_limits)) {
-      par_sum_list <- lapply(par,
-                             calc_par_conc,
-                             min_esd = min_esd,
-                             max_esd = max_esd,
-                             pixel_mm = pixel_mm,
-                             img_vol = img_vol)
-    } else {
-      par_sum_list <- lapply(par,
-                             calc_par_conc_bin,
-                             bin_limits = bin_limits,
-                             pixel_mm = pixel_mm,
-                             img_vol = img_vol)
-    }    
-    names(par_sum_list) <- names(par)
-    class(par_sum_list) <- c('list', 'par_conc_list')
-    return(par_sum_list)
-  
-  } else if(is.etx_class(par, 'par_df')) {
-    
-    if(is.null(pixel_mm) | is.null(img_vol)){
-      stop('Must provide either ecopart_obj or 
-           manually select pixel size and img vol')
-    }
-    #if no bin_limits provided use total method
-    if(is.null(bin_limits)) {
-      par_sum_df <- par |> calc_par_conc(min_esd = min_esd,
-                           max_esd = max_esd,
-                           pixel_mm = pixel_mm,
-                           img_vol = img_vol)
-    } else {
-      par_sum_df <- par |> calc_par_conc_bin(bin_limits = bin_limits,
-                                             pixel_mm = pixel_mm,
-                                             img_vol = img_vol)
-    }
-    return(par_sum_df)
-  }
 }
